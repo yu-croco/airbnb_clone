4e5b6806a299593cab73384d9eb74d9c827f52c0
yu_kitakata <yuki.kita22@gmail.com>
2017-08-09 23:20:13 +0900

    Add: spec test for login/logout with devise

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -4,4 +4,10 @@ module ControllerMacros
 		@request.env["devise.mapping"] = Devise.mappings[:user]
 		sign_in user
 	end
+
+	def logout_user(user)
+		controller.stub(:authenticate_user!).and_return true
+		@request.env["devise.mapping"] = Devise.mappings[:user]
+		sign_out user
+	end
 end
