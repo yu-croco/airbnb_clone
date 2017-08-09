3f54ed74c458f0a7e5df3af610e3d66d6543b888
yu_kitakata <yuki.kita22@gmail.com>
2017-08-09 23:35:54 +0900

    Fix: test case to visit welcome page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get static_pages_index_url
+    get root_path
     assert_response :success
   end
 
