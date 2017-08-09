a3900173ce8ebd39f570a485cdf91db75e43b67c
yu_kitakata <yuki.kita22@gmail.com>
2017-08-09 23:20:13 +0900

    Add: spec test for login/logout with devise

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 	pass = Faker::Internet.password(8)
 
 	factory :user do
-	email		Faker::Internet.email
-	password	pass
+		email		Faker::Internet.email
+		password	pass
 	end
 end
