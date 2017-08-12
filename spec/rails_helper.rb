c07b6f7a12159c855b05e8d7fc06a97fdcef1a12
yu_kitakata <yuki.kita22@gmail.com>
2017-08-12 23:22:37 +0900

    Add: spec test for fb omniauth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,18 @@ RSpec.configure do |config|
 	config.use_transactional_fixtures = true
 	config.infer_spec_type_from_file_location!
 	config.filter_rails_from_backtrace!
-	
+	config.include FacebookMock, type: :controller
 	config.include Devise::TestHelpers, type: :controller
 	config.include ControllerMacros, type: :controller
 end
+
+# force ORM to use the same transaction for all threads (for fb omniauth)
+class ActiveRecord::Base
+	mattr_accessor :shared_connection
+	@@shared_connection = nil
+
+	def self.connection
+		@@shared_connection || retrieve_connection
+	end
+end
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
