05f9c8669674bab821bb80f54e30e2b76aa63f1d
yu_kitakata <yuki.kita22@gmail.com>
2017-08-16 00:16:03 +0900

    Add: basic setting page for listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810164223) do
+ActiveRecord::Schema.define(version: 20170815125811) do
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "house_type"
+    t.integer  "house_years"
+    t.string   "house_size"
+    t.string   "address"
+    t.string   "listing_title"
+    t.text     "listing_content"
+    t.integer  "price_pernight"
+    t.boolean  "active"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
