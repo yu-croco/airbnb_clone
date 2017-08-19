2d41c23742ae2125849e95dfa6ec57c9deb49323
yu_kitakata <yuki.kita22@gmail.com>
2017-08-19 19:37:31 +0900

    Fix: can not see other user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+	before_action :correct_user, only: [:show]
+
 	def show
-		@user = User.find(params[:id])
 	end
+
+	private
+		# before actions
+		def correct_user
+			@user = User.find_by(id: params[:id])
+			if @user != current_user
+				redirect_to root_path, alert: "アクセス権限がありません。"
+			end
+		end
 end
