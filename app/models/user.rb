58b18c716864bba203c976dc95d0937683e31065
yu_kitakata <yuki.kita22@gmail.com>
2017-08-19 23:26:50 +0900

    Add: delete feature for listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 		devise :database_authenticatable, :registerable,
 			:recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-			has_many :listings
+		has_many :listings, dependent: :destroy
 
 		def self.from_omniauth(auth)
 			where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
