dbae582457aae4c3b0a99d5f06acaaac87c00239
yu_kitakata <yuki.kita22@gmail.com>
2017-08-20 15:06:32 +0900

    Fix: uri for devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   config.action_mailer.default_url_options = { host: 'guarded-earth-66849.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: 'murmuring-caverns-39106.herokuapp.com' }
 end
