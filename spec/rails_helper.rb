d8853d8f18b674e71d18fe77e492809a844baae8
yu_kitakata <yuki.kita22@gmail.com>
2017-08-20 12:38:11 +0900

    Modify: dir name of spec integration test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
 	config.use_transactional_fixtures = true
 	config.infer_spec_type_from_file_location!
 	config.filter_rails_from_backtrace!
-	config.include FacebookMock, type: :controller
+	config.include FacebookMock
 	config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
