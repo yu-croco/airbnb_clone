3c11d7c83ff5330f224bb71ec67fd1e227cc68f5
yu-coroco <yuki.kita22@gmail.com>
2017-09-01 01:24:12 +0900

    Delete unnecessary migration file

--- db/migrate/20170827142343_change_column_to_photos.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToPhotos < ActiveRecord::Migration[5.0]
-  def up
-  	change_column :photos, :listing_id, :string, null: false
-  end
-
-  def down
-  	change_column :photos, :listing_id, :string
-  end
-end
