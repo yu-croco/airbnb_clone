883feab94b4b53289573f818e78f949f67baf5ab
yu-coroco <yuki.kita22@gmail.com>
2017-09-10 22:55:56 +0900

    Fix: redirect text for accessing unexistence user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by(id: params[:id])
 		if @user.nil?
-			redirect_to root_path, alert: "リスティング情報は見つかりませんでした。"
+			redirect_to root_path, alert: "ユーザ情報は見つかりませんでした。"
 		end
 	end
 
