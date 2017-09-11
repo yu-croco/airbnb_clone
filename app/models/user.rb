f3983c7eef09ea563c12dc6247c0f8ea00495a47
yu-coroco <yuki.kita22@gmail.com>
2017-09-12 00:12:13 +0900

    Add: missing validation on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
 	has_many :listings, dependent: :destroy
 	has_many :reservations, dependent: :destroy
 
+	validates :email, presence: true, uniqueness: true
+
 	# facebook omniauth
 	def self.from_omniauth(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
