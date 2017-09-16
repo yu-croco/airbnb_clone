1148d911eafef99b87d1955117b26645ce184b00
yu-coroco <yuki.kita22@gmail.com>
2017-09-16 22:45:24 +0900

    Fix: show correct listing on edit/show pages

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,16 +1,23 @@
-// // datepicker for listing reservation calendar
+// datepicker for listing reservation calendar
 $(function() {
 	$("#reservation_start_date").datepicker({
 		format: 'YYYY-MM-DD',
-		language: 'ja'
-	}).on('dp.error', function(e) {
-		$(e.target).val('');
+		language: 'ja',
+		minDate: 0,
+		maxDate: '3m',
+		onSelect: function(selected) {
+			$('#reservation_end_date').datepicker("option", "minDate", selected);
+			$('#reservation_end_date').attr('disabled', false);
+		}
 	});
 
 	$("#reservation_end_date").datepicker({
 		format: 'YYYY-MM-DD',
-		language: 'ja'
-	}).on('dp.error', function(e) {
-		$(e.target).val('');
+		language: 'ja',
+		minDate: 0,
+		maxDate: '3m',
+		onSelect: function(selected) {
+			$('#reservation_start_date').datepicker("option", "maxDate", selected);
+		}
 	});
 });
