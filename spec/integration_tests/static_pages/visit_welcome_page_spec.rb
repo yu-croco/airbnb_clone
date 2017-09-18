2202d8feed9d75300353c98235f37a6118dd0a35
yu-coroco <yuki.kita22@gmail.com>
2017-09-18 13:42:23 +0900

    Fix: delete text on welcome_page for passing spec test

--- spec/integration_tests/static_pages/visit_welcome_page_spec.rb
+++ spec/integration_tests/static_pages/visit_welcome_page_spec.rb
@@ -3,7 +3,7 @@ require'rails_helper'
 describe "can visit welcome page" do
 	it "has a valid factory" do
 		visit root_path
-		expect(page).to have_content 'Hello, world!'
+		expect(page).to have_http_status :success
 	end
 end
 
